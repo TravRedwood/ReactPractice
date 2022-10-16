8d43d38544ab944628f2108eaf751553900c1e5d
travredwood <redwood989@gmail.com>
2022-10-16 16:42:41 -0700

    completed basic temperature counter. stops count ing at 100 and -100 degrease. used: useState hook, inline if/then logic for buttons, inline button JSX,

--- temp_counter/src/App.js
+++ temp_counter/src/App.js
@@ -2,16 +2,54 @@ import React, { useState } from "react";
 
 const App = () => {
   const [tempValue, setTempValue] = useState(30);
+  const [tempColor, setTempColor] = useState("cold");
+
+  const increaseTemp = () => {
+    // first function ran is to check temp value. if value is = 100 the increaseTemp function will return and end
+    // if (tempValue === 100) return;
+
+    const newTemp = tempValue + 10;
+
+    if (newTemp > 50) {
+      setTempColor("hot");
+    }
+
+    setTempValue(newTemp);
+  };
+
+  const decreaseTemp = () => {
+    //if (tempValue === -100) return;
+
+    const newLowerTemp = tempValue - 10;
+
+    if (newLowerTemp < 50) {
+      setTempColor("cold");
+    }
+
+    setTempValue(newLowerTemp);
+  };
+
+  //set inline logic to disable button is temp value is = 100
 
   return (
     <div className="app-container">
       <div className="temperature-display-container">
-        <div className="temperature-display">{tempValue}℉</div>
+        <div className={`temperature-display ${tempColor}`}>{tempValue}℉</div>
       </div>
 
       <div className="button-container">
-        <button onClick={() => setTempValue(tempValue + 10)}>+</button>
-        <button onClick={() => setTempValue(tempValue - 10)}>-</button>
+        <button
+          disabled={tempValue === 100 ? true : false}
+          onClick={() => increaseTemp()}
+        >
+          +
+        </button>
+        <button
+          disabled={tempValue === -100 ? true : false}
+          onClick={() => decreaseTemp()}
+        >
+          -
+        </button>
       </div>
     </div>
   );
