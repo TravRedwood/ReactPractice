c04faac4d098c1d9b9ddb1d6ffe20ca311c8129b
travredwood <redwood989@gmail.com>
2022-10-15 18:46:16 -0700

    created counter app

---
+++ hello-world/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  increase = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrease = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.increase} className="counter">
+          +
+        </button>
+        <button onClick={this.decrease} className="counter">
+          -
+        </button>
+        <h2> {this.state.count}</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
